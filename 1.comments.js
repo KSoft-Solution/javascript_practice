b589ffdf12c35b7da9f39dea559b9969e804a719
KSoft-Solution <kanhasahu955902@gmail.com>
2022-07-31 22:12:48 +0530

    fundamental

---
+++ 1.comments.js
@@ -0,0 +1,15 @@
+//single line comment
+//const value = 1
+
+
+/*
+this is multiline comments
+
+comment 1 
+comment 2
+
+*/
+
+const value = 2 // inline comment
+
+
---
+++ 2.fundamental.js
@@ -0,0 +1,19 @@
+// .js - > javascript extension
+
+
+// .ts - > typescript extension
+// .java - > java extension
+// .php -> php extension
+
+// variable 
+
+// var, let ,const keywords use to make a variable 
+
+var myName = 'ashok sahu'; //variable_declaration(var/let/const) variable_name(myName) = variabl_value('ashok sahu)
+console.log(myName)
+
+// var @name //not work
+// var #name //not work
+// var !name //not work
+
+var $name // work
